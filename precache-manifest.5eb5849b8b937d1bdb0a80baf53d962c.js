4ffc7037d0a0bedb4e339fc7e9fd6a9913caf4f2
Gianpaulo M. Soares <jam_pow@hotmail.com>
2019-05-15 13:50:17 -0300

    Updates

---
+++ precache-manifest.5eb5849b8b937d1bdb0a80baf53d962c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7894ae7e553b5d12987069c56ddcc1d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abd5868cc914b4e5f956",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bca454bdd35d08c250ba",
+    "url": "/static/js/2.b0facd58.chunk.js"
+  },
+  {
+    "revision": "abd5868cc914b4e5f956",
+    "url": "/static/js/main.1e8fd7f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
